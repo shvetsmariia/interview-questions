0619de7355c2ce2466f397c77b132aeece33155a
Mariia <shvetsmariia@yahoo.com>
2023-03-13 20:32:22 -0500

    Added textarea for new question input & checkbox to filter questions

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
+// Creating an array with objects - each object contains an interview question and its status (true or false)
 const interviewQuestions = [
-    {Question: "Explain event delegation", status: true}, 
-    {Question: "What's the difference between a variable that is: null, undefined or undeclared?", status: true}, 
-    {Question: "What are the differences between ES6 class and ES5 function constructors?", status: false},
-    {Question: "Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?", status: true}, 
-    {Question: "Explain 'hoisting'", status: true}, 
-    {Question: "What is the difference between while and do-while loops in JavaScript?", status: false},
-    {Question: "What is the difference between == and ===?", status: true}, 
-    {Question: "What are the differences between variables created using let, var or const?", status: true}, 
-    {Question: "How can you share code between files?", status: false}
+    {question: "Explain event delegation", status: true}, 
+    {question: "What's the difference between a variable that is: null, undefined or undeclared?", status: true}, 
+    {question: "What are the differences between ES6 class and ES5 function constructors?", status: false},
+    {question: "Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?", status: true}, 
+    {question: "Explain 'hoisting'", status: true}, 
+    {question: "What is the difference between while and do-while loops in JavaScript?", status: false},
+    {question: "What is the difference between == and ===?", status: true}, 
+    {question: "What are the differences between variables created using let, var or const?", status: true}, 
+    {question: "How can you share code between files?", status: false}
 ];
 
+// Iterating all of the questions above into console
 interviewQuestions.forEach(interviewQuestion => { console.log(interviewQuestion) }); 
 
+// Creating a function to determine either the status of the question if true or false - in either case assigning an appropriate status icon & description 
 function showStatus(interviewQuestion) {
     if (interviewQuestion.status == true) {
         return `
@@ -29,17 +32,42 @@ function showStatus(interviewQuestion) {
     }
 }
 
-interviewQuestions.forEach(interviewQuestion => { document.getElementById('cards').insertAdjacentHTML('afterend', 
-`
-    <div class="columns is-desktop is-centered">
-    <div class="column is-8">
-    <div class="card">
-    <div class="card-content">
-        <div class="content">${interviewQuestion.Question}</div>
-        <div class="icon-text">${showStatus(interviewQuestion)}</div>
-    </div>
-    </div>
-    </div>
-    </div>
-`);
-})
+// Creating a function that will iterate all questions listed above into a specified HTML format including the question & its status 
+const iterateAllQuestions = function(interviewQuestion, i) {
+    document.getElementById('cards').insertAdjacentHTML('beforeend', 
+        `
+        <div class="columns is-desktop is-centered">
+        <div class="column is-8">
+        <div class="card">
+        <div class="card-content">
+            <div class="content">${i+1}. ${interviewQuestion.question}</div>
+            <div class="icon-text">${showStatus(interviewQuestion)}</div>
+        </div>
+        </div>
+        </div>
+        </div>
+        `
+    );
+}
+
+// Creating a function to iterate the questions and display them into UI 
+function renderIterateAllQuestions() {
+    document.getElementById('cards').innerHTML='';
+    interviewQuestions.forEach(iterateAllQuestions); 
+}
+renderIterateAllQuestions(); 
+
+// Creating a function that will hide questions that I understood and only show the ones that didnot 
+function renderHideQuestionsThatUnderstood() {
+    const arrayQuestionsDidnotUnderstand = interviewQuestions.filter(interviewQuestion => interviewQuestion.status == false); 
+    document.getElementById('cards').innerHTML=''; 
+    arrayQuestionsDidnotUnderstand.forEach(iterateAllQuestions); 
+}
+
+// Creating a function that will add a new question into an array
+function submitNewQuestion() {
+    const newQuestion = document.getElementById('inputQuestion').value; 
+    interviewQuestions.push({'question': newQuestion, status: false});
+    document.getElementById('cards').innerHTML=''; 
+    interviewQuestions.forEach(iterateAllQuestions); 
+}
